76cc9afeecd9db8360c99e2740e20b1a1206c8ec
kimheenimfan <kimheenim.fan@gmail.com>
2018-06-26 22:24:24 -0500

    New Commit

---
+++ automate/hello_world.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+print ("Hello World!!!")
+my_name = input("What is your name? ")
+print ("It's good to meet you, %s" %(my_name))
+print ("The length of your name is:", len(my_name)) 
+my_age = input("What is your age? ")
+print ("You will be "+ str(int(my_age)+1)+" in a year")
